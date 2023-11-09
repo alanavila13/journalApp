04972f2b9354c0a4adb4cdd0c4e441e4d453921d
alanavila13 <tacosricos98@hotmail.com>
2023-11-09 16:32:10 -0600

    class 303 finished

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../store/auth/authSlice";
 import { FirebaseAuth } from "../firebase/config";
+import { startLoadingNotes } from "../store/journal/thunks";
 
 export const useCheckAuth = () => {
   const { status } = useSelector((state) => state.auth);
@@ -13,8 +14,9 @@ export const useCheckAuth = () => {
       if (!user) return dispatch(logout());
       const { uid, email, displayName, photoURL } = user;
       dispatch(login({ uid, email, displayName, photoURL }));
+      dispatch(startLoadingNotes());
     });
   }, []);
 
-  return status
+  return status;
 };
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
+import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector((state) => state.journal);
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {!!active ? <NoteView /> : <NothingSelectedView />}
 
       <IconButton
+        onClick={onClickNewNote}
+        disabled={isSaving}
         size="large"
         sx={{
           color: "white ",
@@ -19,7 +29,7 @@ export const JournalPage = () => {
           bottom: 50,
         }}
       >
-        <AddOutlined sx={{ fontSize: 30}} />
+        <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
       {/* <NoteView /> */}
     </JournalLayout>
